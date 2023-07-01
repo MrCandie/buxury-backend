5a7572210a7f825a0832fd6448cf435fb0342e9e
mrcandie~ <akinwumiadekanmi2@gmail.com>
2023-07-01 15:23:55 +0100

    another commit

--- router/store-router.js
+++ router/store-router.js
@@ -11,10 +11,10 @@ const { setUserId, protect } = require("../middleware");
 const router = express.Router();
 
 router.post("/", protect, setUserId, createStore);
+router.get("/all", getAllStores);
 
 router.get("/me", protect, getUserStores);
 router.get("/:slug", protect, viewStore);
-router.get("/all", getAllStores);
 
 router.patch("/me/update/:id", protect, updateStore);
 
