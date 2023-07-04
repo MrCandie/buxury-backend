bf519ebab0d6574a13fc48bad8c928c12d9d41d9
mrcandie~ <akinwumiadekanmi2@gmail.com>
2023-07-04 16:27:25 +0100

    nother commit

--- model/review-model.js
+++ model/review-model.js
@@ -17,6 +17,14 @@ const reviewSchema = new mongoose.Schema(
       type: String,
       required: [true, "favorite must belong to a product"],
     },
+    review: {
+      type: String,
+      required: [true, "add a comment"],
+    },
+    rating: {
+      type: Number,
+      required: [true, "add a rating"],
+    },
     createdAt: {
       type: Date,
       default: Date.now(),
